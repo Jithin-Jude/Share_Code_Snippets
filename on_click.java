5025cfed7746eacefd94d82812fe4deedffe427a
Jithin K Jose <jithin.jude68@gmail.com>
2018-05-30 19:17:15 +0530

    Create on_click.java

---
+++ on_click.java
@@ -0,0 +1,8 @@
+        ListView listView = findViewById(R.id.sandwiches_listview);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                launchDetailActivity(position);
+            }
+        });
